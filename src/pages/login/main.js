97e965956e197877e248fa365282e42f0710600b
Codrut Marin | Cross Point <Codrut.Marin@crosspoint.local>
2019-05-01 17:02:17 +0200

    Initial commit

---
+++ src/pages/login/main.js
@@ -0,0 +1,69 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     height: "20vh",
+//     minHeight: "150px",
+//   },
+//   control: {
+//     padding: theme.spacing.unit * 2,
+//   },
+// });
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div id="content">
+        <Grid container className="root" spacing={8}>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+
+/*
+
+
+          <Grid item xs={12} className="header">
+              <Grid container spacing={8}>
+                  <Grid item xs={12}>
+                      <Paper className="paper" > </Paper>
+                  </Grid>
+              </Grid>
+          </Grid>
+          <Grid item xs={12} className="body">
+              <Grid container spacing={8}>
+                  <Grid item xs={4}>
+                      <Paper className="paper" > </Paper>
+                  </Grid>
+                  <Grid item xs={4}>
+                      <Paper className="paper" > </Paper>
+                  </Grid>
+                  <Grid item xs={4}>
+                      <Paper className="paper" > </Paper>
+                  </Grid>
+              </Grid>
+          </Grid>
+          <Grid item xs={12} className="header">
+              <Grid container spacing={8}>
+                  <Grid item xs={12}>
+                      <Paper className="paper" > </Paper>
+                  </Grid>
+              </Grid>
+          </Grid>
+
+*/
+// Main.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(Main);
+export default Main;
