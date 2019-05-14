3b9bc403c88ba21824335d269aaee80affa99e94
Codrut Marin <codrutsoft13@gmail.com>
2019-05-14 14:32:26 +0200

    server modifier

--- src/pages/chat/socket_io.js
+++ src/pages/chat/socket_io.js
@@ -1,4 +1,5 @@
 import openSocket from 'socket.io-client';
+import * as prepareUrls from "local-ip-url/prepareUrls" ;
 // const socket =  openSocket("http://localhost:8000");
 // socket.on('custom_event_response', this.eventResponse);
 // socket.emit('new_user', obj); 
@@ -13,7 +14,7 @@ class SocketClient {
     getInstace()
     {
         if(this.socket == null) {
-            this.socket = openSocket("http://localhost:8000");
+            this.socket = openSocket("http://"+ prepareUrls().ip +":5000");
         }
         return this.socket;
     }
